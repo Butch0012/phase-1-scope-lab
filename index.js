ec5cc9fa3433daea3dcc5987659ad2b89ce48462
Elnathan Mburu <elnathanmburu@gmail.com>
2023-10-08 06:23:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = 'bob' //declaring variable
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()//Changing to uppercase
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';      // Output is not bob
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'        // Output is maybe bob
+}
+
+const leastFavoriteCustomer = "";
+//testing
+function changeLeastFavoriteCustomer() { 
+  leastFavoriteCustomer = 'test'
+}
+
